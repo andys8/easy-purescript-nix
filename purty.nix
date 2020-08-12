276e662a356eb1656cbe7675daf91c0903e8c3d8
andys8 <andys8@users.noreply.github.com>
2020-08-12 18:20:43 +0200

    WIP Purty v6.2.0

--- purty.nix
+++ purty.nix
@@ -15,16 +15,16 @@ in
 pkgs.stdenv.mkDerivation rec {
   pname = "purty";
 
-  version = "4.5.1";
+  version = "6.2.0";
 
   src = if pkgs.stdenv.isDarwin
   then pkgs.fetchurl {
-    url = "https://bintray.com/joneshf/generic/download_file?file_path=purty-4.5.1-osx.tar.gz";
-    sha256 = "1nl86ajix0kzz7l6my1nj22zra4pcz7mp6kb730p2a9jxdk37f12";
+    url = "https://bintray.com/joneshf/generic/download_file?file_path=purty-6.2.0-osx.tar.gz";
+    sha256 = "1nl87ajix0kzz7l6my1nj22zra4pcz7mp6kb730p2a9jxdk37f12";
   }
   else pkgs.fetchurl {
-    url = "https://bintray.com/joneshf/generic/download_file?file_path=purty-4.5.1-linux.tar.gz";
-    sha256 = "050m7wnaz7d20amsprps02j65qywa4r0n873f444g6db9alvazrv";
+    url = "https://bintray.com/joneshf/generic/download_file?file_path=purty-6.2.0-linux.tar.gz";
+    sha256 = "052m7wnaz7d20amsprps02j65qywa4r0n873f444g6db9alvazrv";
   };
 
   buildInputs = [ pkgs.zlib pkgs.gmp pkgs.ncurses5 ];
